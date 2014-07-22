157637b2da89d145080a95d781308473f7e96247
Andy Gayton <andy@thecablelounge.com>
2014-07-21 18:54:22 -0700

    reorder tests

--- tests/unit/test_vanilla.py
+++ tests/unit/test_vanilla.py
@@ -22,17 +22,93 @@ class TestHub(object):
 
 
 class TestPiping(object):
-    def test_stream(self):
+    def test_timeout(self):
         h = vanilla.Hub()
 
-        @h.stream
-        def counter(sender):
-            for i in xrange(10):
-                sender.send(i)
+        sender, recver = h.pipe()
+        check_sender, check_recver = h.pipe()
 
-        assert counter.recv() == 0
-        h.sleep(10)
-        assert counter.recv() == 1
+        pytest.raises(vanilla.Timeout, sender.send, 12, timeout=0)
+        pytest.raises(vanilla.Timeout, recver.recv, timeout=0)
+        pytest.raises(vanilla.Timeout, sender.send, 12, timeout=0)
+
+        @h.spawn
+        def _():
+            h.sleep(20)
+            check_sender.send(recver.recv())
+
+        pytest.raises(vanilla.Timeout, sender.send, 12, timeout=10)
+        sender.send(12, timeout=20)
+        assert check_recver.recv() == 12
+
+        @h.spawn
+        def _():
+            h.sleep(20)
+            sender.send(12)
+
+        pytest.raises(vanilla.Timeout, recver.recv, timeout=10)
+        assert recver.recv(timeout=20) == 12
+
+    def test_select(self):
+        h = vanilla.Hub()
+
+        s1, r1 = h.pipe()
+        s2, r2 = h.pipe()
+        check_s, check_r = h.pipe()
+
+        @h.spawn
+        def _():
+            check_s.send(r1.recv())
+
+        @h.spawn
+        def _():
+            s2.send(10)
+            check_s.send('done')
+
+        ch, item = h.select([s1, r2])
+        assert ch == s1
+        s1.send(20)
+
+        ch, item = h.select([s1, r2])
+        assert ch == r2
+        assert item == 10
+
+        assert check_r.recv() == 20
+        assert check_r.recv() == 'done'
+
+    def test_select_timeout(self):
+        h = vanilla.Hub()
+
+        s1, r1 = h.pipe()
+        s2, r2 = h.pipe()
+        check_s, check_r = h.pipe()
+
+        pytest.raises(vanilla.Timeout, h.select, [s1, r2], timeout=0)
+
+        @h.spawn
+        def _():
+            h.sleep(20)
+            check_s.send(r1.recv())
+
+        pytest.raises(vanilla.Timeout, h.select, [s1, r2], timeout=10)
+
+        ch, item = h.select([s1, r2], timeout=20)
+        assert ch == s1
+        s1.send(20)
+        assert check_r.recv() == 20
+
+        @h.spawn
+        def _():
+            h.sleep(20)
+            s2.send(10)
+            check_s.send('done')
+
+        pytest.raises(vanilla.Timeout, h.select, [s1, r2], timeout=10)
+
+        ch, item = h.select([s1, r2], timeout=20)
+        assert ch == r2
+        assert item == 10
+        assert check_r.recv() == 'done'
 
     def test_abandoned_sender(self):
         h = vanilla.Hub()
@@ -96,6 +172,18 @@ class TestPiping(object):
         gc.collect()
         assert check_recver.recv() == 'done'
 
+    def test_stream(self):
+        h = vanilla.Hub()
+
+        @h.stream
+        def counter(sender):
+            for i in xrange(10):
+                sender.send(i)
+
+        assert counter.recv() == 0
+        h.sleep(10)
+        assert counter.recv() == 1
+
     def test_pulse(self):
         h = vanilla.Hub()
 
@@ -111,94 +199,6 @@ class TestPiping(object):
         pytest.raises(vanilla.Timeout, trigger.recv, timeout=0)
 
         # TODO: test abandoned
-
-    def test_select(self):
-        h = vanilla.Hub()
-
-        s1, r1 = h.pipe()
-        s2, r2 = h.pipe()
-        check_s, check_r = h.pipe()
-
-        @h.spawn
-        def _():
-            check_s.send(r1.recv())
-
-        @h.spawn
-        def _():
-            s2.send(10)
-            check_s.send('done')
-
-        ch, item = h.select([s1, r2])
-        assert ch == s1
-        s1.send(20)
-
-        ch, item = h.select([s1, r2])
-        assert ch == r2
-        assert item == 10
-
-        assert check_r.recv() == 20
-        assert check_r.recv() == 'done'
-
-    def test_select_timeout(self):
-        h = vanilla.Hub()
-
-        s1, r1 = h.pipe()
-        s2, r2 = h.pipe()
-        check_s, check_r = h.pipe()
-
-        pytest.raises(vanilla.Timeout, h.select, [s1, r2], timeout=0)
-
-        @h.spawn
-        def _():
-            h.sleep(20)
-            check_s.send(r1.recv())
-
-        pytest.raises(vanilla.Timeout, h.select, [s1, r2], timeout=10)
-
-        ch, item = h.select([s1, r2], timeout=20)
-        assert ch == s1
-        s1.send(20)
-        assert check_r.recv() == 20
-
-        @h.spawn
-        def _():
-            h.sleep(20)
-            s2.send(10)
-            check_s.send('done')
-
-        pytest.raises(vanilla.Timeout, h.select, [s1, r2], timeout=10)
-
-        ch, item = h.select([s1, r2], timeout=20)
-        assert ch == r2
-        assert item == 10
-        assert check_r.recv() == 'done'
-
-    def test_timeout(self):
-        h = vanilla.Hub()
-
-        sender, recver = h.pipe()
-        check_sender, check_recver = h.pipe()
-
-        pytest.raises(vanilla.Timeout, sender.send, 12, timeout=0)
-        pytest.raises(vanilla.Timeout, recver.recv, timeout=0)
-        pytest.raises(vanilla.Timeout, sender.send, 12, timeout=0)
-
-        @h.spawn
-        def _():
-            h.sleep(20)
-            check_sender.send(recver.recv())
-
-        pytest.raises(vanilla.Timeout, sender.send, 12, timeout=10)
-        sender.send(12, timeout=20)
-        assert check_recver.recv() == 12
-
-        @h.spawn
-        def _():
-            h.sleep(20)
-            sender.send(12)
-
-        pytest.raises(vanilla.Timeout, recver.recv, timeout=10)
-        assert recver.recv(timeout=20) == 12
 
     """
     def test_buffer(self):
