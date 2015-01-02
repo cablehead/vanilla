c8315d67f84a04307179784d2017931f1124234e
Andy Gayton <andy@thecablelounge.com>
2015-01-02 15:15:00 -0800

    rework ticktock example

--- examples/ticktock.py
+++ examples/ticktock.py
@@ -2,13 +2,16 @@ import vanilla
 
 h = vanilla.Hub()
 
+r = h.router()
+
 
 def beat(message):
     while True:
-        print(message)
+        r.send(message)
         h.sleep(1000)
 
 h.spawn(beat, 'Tick')
 h.spawn_later(500, beat, 'Tock')
 
-h.stop_on_term()
+for message in r.recver:
+    print(message)
