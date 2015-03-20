f2625250112df2cba47d777d08ea8aed6a3a29f4
Andy Gayton <andy@thecablelounge.com>
2015-03-19 20:42:02 -0700

    don't assume output from grep

--- tests/unit/test_process.py
+++ tests/unit/test_process.py
@@ -15,10 +15,10 @@ class TestProcess(object):
     def test_stderr(self):
         h = vanilla.Hub()
         child = h.process.execv(['/usr/bin/env', 'grep', '-g'])
-        assert child.stderr.recv() == "grep: invalid option -- 'g'\n"
+        assert child.stderr.recv()
 
     def test_stderrtoout(self):
         h = vanilla.Hub()
         child = h.process.execv(
             ['/usr/bin/env', 'grep', '-g'], stderrtoout=True)
-        assert child.stdout.recv() == "grep: invalid option -- 'g'\n"
+        assert child.stdout.recv()
