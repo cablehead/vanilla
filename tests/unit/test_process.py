adca7b7c7a66af023fe25c4fecf2543c1b016017
Andy Gayton <andy@thecablelounge.com>
2015-03-20 04:20:05 -0700

    make use of env to locate grep

--- tests/unit/test_process.py
+++ tests/unit/test_process.py
@@ -3,7 +3,7 @@ import vanilla
 
 def test_process():
     h = vanilla.Hub()
-    child = h.process.execv(['/bin/grep', '--line-buffered', 'foo'])
+    child = h.process.execv(['/usr/bin/env', 'grep', '--line-buffered', 'foo'])
     child.stdin.send('foo1\n')
     assert child.stdout.recv() == 'foo1\n'
     child.stdin.send('bar1\n')
