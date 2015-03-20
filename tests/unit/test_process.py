ddead0781503377bf7287cb053c1141fb528d838
Andy Gayton <andy@thecablelounge.com>
2015-03-19 20:18:55 -0700

    add a test for stderr

--- tests/unit/test_process.py
+++ tests/unit/test_process.py
@@ -1,11 +1,18 @@
 import vanilla
 
 
-def test_process():
-    h = vanilla.Hub()
-    child = h.process.execv(['/usr/bin/env', 'grep', '--line-buffered', 'foo'])
-    child.stdin.send('foo1\n')
-    assert child.stdout.recv() == 'foo1\n'
-    child.stdin.send('bar1\n')
-    child.stdin.send('foo2\n')
-    assert child.stdout.recv() == 'foo2\n'
+class TestProcess(object):
+    def test_basic(self):
+        h = vanilla.Hub()
+        child = h.process.execv(
+            ['/usr/bin/env', 'grep', '--line-buffered', 'foo'])
+        child.stdin.send('foo1\n')
+        assert child.stdout.recv() == 'foo1\n'
+        child.stdin.send('bar1\n')
+        child.stdin.send('foo2\n')
+        assert child.stdout.recv() == 'foo2\n'
+
+    def test_stderr(self):
+        h = vanilla.Hub()
+        child = h.process.execv(['/usr/bin/env', 'grep', '-g'])
+        assert child.stderr.recv() == "grep: invalid option -- 'g'\n"
