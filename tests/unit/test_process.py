6840c1f9312fabfaba7f185b2000f4ada35981d7
Andy Gayton <andy@thecablelounge.com>
2015-04-02 12:35:48 -0700

    add a small sleep before testing if child process stops

--- tests/unit/test_process.py
+++ tests/unit/test_process.py
@@ -3,22 +3,20 @@ import signal
 
 
 class TestProcess(object):
-    def test_signal(self):
-        h = vanilla.Hub()
-        child = h.process.execv(
-            ['/usr/bin/env', 'grep', '--line-buffered', 'foo'])
-        child.signal(signal.SIGTERM)
-        assert not child.check_liveness()
-
     def test_basic(self):
         h = vanilla.Hub()
         child = h.process.execv(
             ['/usr/bin/env', 'grep', '--line-buffered', 'foo'])
+        assert child.check_liveness()
         child.stdin.send('foo1\n')
         assert child.stdout.recv() == 'foo1\n'
         child.stdin.send('bar1\n')
         child.stdin.send('foo2\n')
         assert child.stdout.recv() == 'foo2\n'
+        child.terminate()
+        # give child a chance to die
+        h.sleep(10)
+        assert not child.check_liveness()
 
     def test_stderr(self):
         h = vanilla.Hub()
@@ -30,3 +28,12 @@ class TestProcess(object):
         child = h.process.execv(
             ['/usr/bin/env', 'grep', '-g'], stderrtoout=True)
         assert child.stdout.recv()
+
+    def test_signal(self):
+        h = vanilla.Hub()
+        child = h.process.execv(
+            ['/usr/bin/env', 'grep', '--line-buffered', 'foo'])
+        child.signal(signal.SIGTERM)
+        # give child a chance to die
+        h.sleep(10)
+        assert not child.check_liveness()
--- vanilla/process.py
+++ vanilla/process.py
@@ -46,7 +46,7 @@ class __plugin__(object):
             self.hub = hub
             self.pid = pid
             # TODO: should use an event here
-            self.done = self.hub.channel()
+            # self.done = self.hub.channel()
 
         def check_liveness(self):
             try:
@@ -59,7 +59,7 @@ class __plugin__(object):
 
             self.exitcode = code >> 8
             self.exitsignal = code & (2**8-1)
-            self.done.send(self)
+            # self.done.send(self)
             return False
 
         def terminate(self):
