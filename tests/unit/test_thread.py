d26c187194d15f617a0baa957418d8c1fa638693
Andy Gayton <andy@thecablelounge.com>
2015-06-20 23:14:44 -0700

    add a thread pool

--- tests/unit/test_thread.py
+++ tests/unit/test_thread.py
@@ -1,5 +1,9 @@
+import time
+
 import vanilla
 
+
+# TODO: test shutdown
 
 def test_call():
     def add(a, b):
@@ -7,3 +11,22 @@ def test_call():
 
     h = vanilla.Hub()
     assert h.thread.call(add, 2, 3).recv() == 5
+
+
+def test_pool():
+    h = vanilla.Hub()
+
+    def sleeper(x):
+        time.sleep(x)
+        return x
+
+    p = h.thread.pool(2)
+    check = h.router()
+
+    p.call(sleeper, 0.2).pipe(check)
+    p.call(sleeper, 0.1).pipe(check)
+    p.call(sleeper, 0.05).pipe(check)
+
+    assert check.recv() == 0.1
+    assert check.recv() == 0.05
+    assert check.recv() == 0.2
--- vanilla/thread.py
+++ vanilla/thread.py
@@ -1,11 +1,16 @@
+import collections
 import threading
 import os
 
+from Queue import Queue
+
+from vanilla.exception import Closed
+
 
 class Oneshot(object):
-    def __init__(self, h, f, a):
+    def __init__(self, hub, f, a):
         pipe_r, self.pipe_w = os.pipe()
-        self.recver = h.io.fd_in(pipe_r).map(self.done)
+        self.recver = hub.io.fd_in(pipe_r).map(self.done)
         self.t = threading.Thread(target=self.run, args=(f, a))
         self.t.start()
 
@@ -17,9 +22,73 @@ class Oneshot(object):
         os.write(self.pipe_w, chr(1))
 
 
+class Pool(object):
+    def __init__(self, hub, size):
+        self.hub = hub
+        self.size = size
+
+        pipe_r, self.pipe_w = os.pipe()
+        self.pipe_r = hub.io.fd_in(pipe_r)
+
+        self.requests = Queue()
+        self.results = collections.deque()
+        self.closed = False
+        self.threads = 0
+
+        for i in xrange(size):
+            t = threading.Thread(target=self.runner)
+            t.daemon = True
+            t.start()
+            self.threads += 1
+
+        hub.spawn(self.responder)
+
+    def runner(self):
+        while True:
+            item = self.requests.get()
+            if type(item) == Closed:
+                self.threads -= 1
+                if self.threads <= 0:
+                    # entire pool has stopped, send signal
+                    os.write(self.pipe_w, chr(0))
+                return
+            sender, f, a, kw = item
+            result = f(*a, **kw)
+            self.requests.task_done()
+            self.results.append((sender, result))
+            # send signal to wake up main thread
+            os.write(self.pipe_w, chr(1))
+
+    def responder(self):
+        for s in self.pipe_r:
+            for ch in s:
+                ch = ord(ch)
+                if not ch:
+                    break
+                sender, result = self.results.popleft()
+                sender.send(result)
+        self.pipe_r.close()
+
+    def call(self, f, *a, **kw):
+        if self.closed:
+            raise Closed
+        sender, recver = self.hub.pipe()
+        self.requests.put((sender, f, a, kw))
+        return recver
+
+    def close(self):
+        self.closed = True
+        for i in xrange(self.size):
+            # tell thread pool to stop when they have finished the last request
+            self.requests.put(Closed())
+
+
 class __plugin__(object):
     def __init__(self, hub):
         self.hub = hub
 
     def call(self, f, *a):
         return Oneshot(self.hub, f, a).recver
+
+    def pool(self, size):
+        return Pool(self.hub, size)
